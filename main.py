19382a313a83fa09d257b880adb51c19b8d05a4d
EduardIbatullin <e.ibatullin@gmail.com>
2024-05-08 23:32:46 +0500

    adding new functionality

--- main.py
+++ main.py
@@ -1,38 +1,96 @@
 import pygame
 import random
+import math
 
-pygame.init()  # инициализация PyGame
+pygame.init()
 
-SCREEN_WIDTH = 800  # ширина экрана
-SCREEN_HEIGHT = 600  # высота экрана
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # размеры экрана
+# Константы
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+TARGET_WIDTH = 80
+TARGET_HEIGHT = 80
 
-pygame.display.set_caption("Игра Тир")  # заголовок окна
-icon = pygame.image.load('img/duck_hunt.png')  # иконка окна
-pygame.display.set_icon(icon)  # иконка окна
+# Загрузка изображений
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Игра Утиная охота")
+icon = pygame.image.load('img/duck_hunt.png')
+pygame.display.set_icon(icon)
+duck_image = pygame.image.load('img/duck.png')
+cloud_image = pygame.image.load('img/cloud.png')  # Изображение облака перьев
 
-target_image = pygame.image.load('img/target.png')  # картинка цели
-target_width = 80  # ширина картинки цели
-target_height = 80  # высота картинки цели
+# Начальные параметры цели
+target_x = -TARGET_WIDTH
+target_y_start = SCREEN_HEIGHT // 2
+amplitude = random.randint(50, 250)
+frequency = 0.01
 
-target_x = random.randint(0, SCREEN_WIDTH - target_width)  # координаты цели по оси X
-target_y = random.randint(0, SCREEN_HEIGHT - target_height)  # координаты цели по оси Y
+# Загрузка изображения фона
+background_image = pygame.image.load('img/background.jpg').convert()
 
-color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))  # цвет фона окна
+# Установка курсора прицела
+pygame.mouse.set_visible(False)  # Скрываем стандартный курсор
+crosshair_image = pygame.image.load('img/crosshair.png').convert_alpha()
 
-# цикл обработки событий игры
-running = True
-while running:
-    screen.fill(color)  # заливка фона окна цвет
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:  # если нажали на крестик
-            running = False  # выход из цикла обработки событий игры
-        elif event.type == pygame.MOUSEBUTTONDOWN:  # если нажали на мышь
-            mouse_x, mouse_y = pygame.mouse.get_pos()  # координаты курсора
-            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
-                target_x = random.randint(0, SCREEN_WIDTH - target_width)  # координаты цели по оси X изменились
-                target_y = random.randint(0, SCREEN_HEIGHT - target_height)  # координаты цели по оси Y изменились
-    screen.blit(target_image, (target_x, target_y))  # вывод картинки цели на экран
-    pygame.display.update()  # обновление экрана
+# Звук выстрела
+shoot_sound = pygame.mixer.Sound('sounds/shoot.wav')
 
-pygame.quit()
+
+def update_target():
+    global target_x, target_y_start, amplitude, frequency
+
+    target_x += 5
+    target_y = target_y_start + amplitude * math.sin(frequency * target_x)
+
+    if target_x > SCREEN_WIDTH:
+        target_x = -TARGET_WIDTH
+        target_y_start = random.randint(SCREEN_HEIGHT // 4, 3 * SCREEN_HEIGHT // 4)
+        amplitude = random.randint(50, 150)
+        frequency = 0.005
+
+    screen.blit(duck_image, (target_x, target_y))
+
+    return (target_x, target_y)
+
+
+def main():
+    global target_x, target_y_start, amplitude, frequency
+    running = True
+    clock = pygame.time.Clock()
+    duck_hit = False  # Флаг для отслеживания попадания в утку
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                pygame.mixer.Sound.play(shoot_sound)
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                target_pos = update_target()
+                if target_pos[0] <= mouse_x <= target_pos[0] + TARGET_WIDTH and \
+                        target_pos[1] <= mouse_y <= target_pos[1] + TARGET_HEIGHT:
+                    duck_hit = True  # Установка флага, если утка была убита
+                    screen.blit(cloud_image, (target_pos[0], target_pos[1]))
+                    pygame.display.flip()
+                    pygame.time.wait(1000)  # Ожидание 1 секунды
+
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        screen.blit(background_image, (0, 0))  # Отрисовываем фон
+        if not duck_hit:
+            update_target()  # Обновление положения утки, если она не была убита
+        else:
+            duck_hit = False  # Сброс флага
+            target_x = -TARGET_WIDTH  # Сброс позиции утки для появления новой с левого края экрана
+            target_y_start = random.randint(SCREEN_HEIGHT // 4, 3 * SCREEN_HEIGHT // 4)
+            amplitude = random.randint(50, 150)
+            frequency = 0.005
+
+        screen.blit(crosshair_image,
+                    (mouse_x - crosshair_image.get_width() // 2, mouse_y - crosshair_image.get_height() // 2))
+
+        pygame.display.flip()
+        clock.tick(60)
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
